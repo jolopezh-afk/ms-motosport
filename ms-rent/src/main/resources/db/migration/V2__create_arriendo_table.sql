d0be34cd7075dab750c3c86ea0798e6f6e4dabbb
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-08 19:54:37 -0400

    Refactorizado

---
+++ ms-rent/src/main/resources/db/migration/V2__create_arriendo_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE rent (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    moto_id BIGINT NOT NULL,
+    cliente_id BIGINT NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    observacion VARCHAR(255)
+);
